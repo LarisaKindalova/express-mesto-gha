a2794c34a4f7d1d0e18be6a5661c0b261b4d422c
Larisa Kindalova <l.kindalova@yandex.ru>
2023-07-19 16:50:48 +1000

    fix: user profile & avatar

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,6 @@ module.exports.getUserById = (req, res) => {
     .then((user) => res.status(OK).send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
-        console.log('не нашли');
         return res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные', err: err.message, stack: err.stack });
       }
       if (err.name === 'DocumentNotFoundError') {
@@ -43,28 +42,30 @@ module.exports.getUserById = (req, res) => {
 
 module.exports.updateUserInfo = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail()
     .then((user) => res.status(OK).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST).send({ message: 'Переданны некорретные даные при создании пользователя', err: err.message, stack: err.stack });
-      } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка', err: err.message, stack: err.stack });
+        return res.status(BAD_REQUEST).send({ message: 'Переданны некорретные даные при обновлении данных пользователя', err: err.message, stack: err.stack });
+      } if (err.name === 'DocumentNotFoundError') {
+        return res.status(NOT_FOUND).send({ message: 'Пользователь с указанным id не найден', err: err.message, stack: err.stack });
       }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка', err: err.message, stack: err.stack });
     });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail()
     .then((user) => res.status(OK).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(BAD_REQUEST).send({ message: 'Переданны некорретные даные при создании пользователя', err: err.message, stack: err.stack });
-      } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка', err: err.message, stack: err.stack });
+        return res.status(BAD_REQUEST).send({ message: 'Переданны некорретные даные при обновлении аватара', err: err.message, stack: err.stack });
+      } if (err.name === 'DocumentNotFoundError') {
+        return res.status(NOT_FOUND).send({ message: 'Пользователь с указанным id не найден', err: err.message, stack: err.stack });
       }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка', err: err.message, stack: err.stack });
     });
 };
