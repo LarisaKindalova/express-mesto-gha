e1e5580b9deba0aee693182d0e5610952f72db2f
Larisa Kindalova <l.kindalova@yandex.ru>
2023-07-31 11:42:35 +1000

    faeat: card validation

--- routes/cards.js
+++ routes/cards.js
@@ -2,12 +2,12 @@ const cardRouter = require('express').Router();
 const {
   createCard, getCards, deleteCard, putCardLike, deleteCardLike,
 } = require('../controllers/cards');
-const { validateCreateCard } = require('../middlewares/validate');
+const { validateCreateCard, validateCardId } = require('../middlewares/validate');
 
 cardRouter.get('/', getCards);
-cardRouter.delete('/:cardId', deleteCard);
+cardRouter.delete('/:cardId', validateCardId, deleteCard);
 cardRouter.post('/', validateCreateCard, createCard);
-cardRouter.put('/:cardId/likes', putCardLike);
-cardRouter.delete('/:cardId/likes', deleteCardLike);
+cardRouter.put('/:cardId/likes', validateCardId, putCardLike);
+cardRouter.delete('/:cardId/likes', validateCardId, deleteCardLike);
 
 module.exports = cardRouter;
